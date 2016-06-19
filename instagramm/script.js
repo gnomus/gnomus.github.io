56f80e8c96593f51734d8f49c2f6baa5f16bd95d
Felix Favre <gnomus@gnomus.de>
2016-06-19 19:27:00 +0200

    asdf

--- instagramm/script.js
+++ instagramm/script.js
@@ -1,17 +1,17 @@
 function changeSrc (img) {
 	var imgid = Math.floor(Math.random()*5) + 1
 	img.attr("src", "img/" + imgid+".jpg")
-	
+
 	//img.fadeTo(200,0.30, function() {
 	//	img.attr("src", "img/" + imgid+".jpg")
 	//}).fadeTo(200,1);
-	
+
 	console.log("X")
 }
 
-var parentdiv =  $("#background");
-for (var i = 0; i < 39; i++) {
-	for (var j = 0; j < 22; j++)
+var parentdiv =  $(".content.uploads");
+for (var i = 0; i < 24; i++) {
+	for (var j = 0; j < 16; j++)
 	{
 		var img = $("<img>");
 		img.attr("src", "img/white.jpg")
@@ -28,4 +28,4 @@ for (var i = 0; i < 39; i++) {
 			};
 		}(img), Math.random()*1000);
 	}
-} 
+}
